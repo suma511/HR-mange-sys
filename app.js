c6cdf2591b4cd00bf261d0000d2c52a13396066c
suma511 <eliansumaia@gmail.com>
2022-01-25 23:45:49 +0200

    uncompleted

---
+++ app.js
@@ -0,0 +1,17 @@
+function takevalue()
+{
+    let fname=document.getElementById("fname").value;
+    let lname=document.getElementById("lname").value; 
+    let department=document.getElementById("department").value;
+    let level=document.getElementById("level").value;
+    document.write("first name : " + fname +""); 
+    document.write("second name : " + lname +""); 
+    document.write(" department : " + department +""); 
+    document.write("level : " + level +""); 
+    document.write("image : " + image +""); 
+
+}
+function func(){
+let idUn = new Date().getTime.toString();
+console.log(idUn)
+}
